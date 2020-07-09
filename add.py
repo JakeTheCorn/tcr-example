7b5877cf501acc5f7f650314bb8b0380062a668c
JakeTheCorn <jakethecorn@gmail.com>
2020-07-09 17:27:07 -0400

    working

--- add.py
+++ add.py
@@ -4,7 +4,8 @@ def add(*addends):
         acc = acc + addend
     return acc
 
-
+def subtract_from(*nums):
+    return 1
 
 assert True
 assert add(1, 2) == 3
@@ -12,4 +13,4 @@ assert add(1, 2, 3) == 6
 assert add(1) == 1
 assert add(3, 4) == 7
 assert True
-
+assert subtract_from(1, 0) == 1
