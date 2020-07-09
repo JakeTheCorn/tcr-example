bdba59136a82cea1851a7aac2593a86d765e4d4e
JakeTheCorn <jakethecorn@gmail.com>
2020-07-09 17:42:14 -0400

    working

--- add.py
+++ add.py
@@ -11,7 +11,7 @@ class Test(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3, 4), 7)
         self.assertEqual(add(3, 3), 6)
-        self.assertEqual(add(3, 2), 5)
+        self.assertEqual(add(3, 2, 1), 6)
 
 
 if __name__ == "__main__":
