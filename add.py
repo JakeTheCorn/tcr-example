1cabd4fbb4ef587895d8cc81087c0ea99c48de59
JakeTheCorn <jakethecorn@gmail.com>
2020-07-09 17:41:22 -0400

    working

--- add.py
+++ add.py
@@ -6,17 +6,16 @@ def add(*addends):
         acc = acc + addend
     return acc
 
-def subtract_from(*nums):
-    if len(nums) == 1:
-        return nums[0]
-    return 1
+
+
+class Test(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(3, 4), 7)
 
 assert add(1, 2) == 3
 assert add(1, 2, 3) == 6
 assert add(1) == 1
 assert add(3, 4) == 7
-assert subtract_from(1, 0) == 1
-assert subtract_from(1) == 1
 
 if __name__ == "__main__":
     unittest.main()
